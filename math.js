5a1b557b87ea86a0fe09126601bc639b1ceb6a2e
JOY DASH <joydash5778@gmail.com>
2023-08-05 20:48:04 +0600

    math js file updated

---
+++ math.js
@@ -0,0 +1,18 @@
+/*
+Method of math :
+Math.pow means any number up square 
+Math.abs means 
+Math.max
+Math.min
+Math.round
+Math.ceil
+Math.floor
+Math.random
+*/
+
+//ex of math round in 20 times loop counting 0/6 :
+
+for(let i=0; i < 20; i++){
+    const random = Math.round(Math.random()*6);
+    console.log(random);
+}
