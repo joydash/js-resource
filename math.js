12288aea8f1bad843f14455cf47ad011b8b989e5
JOY DASH <joydash5778@gmail.com>
2023-08-06 01:02:43 +0600

    update math js

--- math.js
+++ math.js
@@ -15,4 +15,15 @@ Math.random
 for(let i=0; i < 20; i++){
     const random = Math.round(Math.random()*6);
     console.log(random);
+
+
 }
+
+
+//problem solving 
+
+let num1 = 67;
+let num2 = 97;
+let num3 = 99;
+let win = Math.max(num1, num2, num3);
+console.log(win)
