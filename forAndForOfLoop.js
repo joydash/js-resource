047239c051467f9c8028e3f7479de7dc6fa8f954
JOY DASH <joydash5778@gmail.com>
2023-08-08 00:01:00 +0600

    added for in loop function

--- forAndForOfLoop.js
+++ forAndForOfLoop.js
@@ -7,8 +7,27 @@ for(let i = 0; i < products.length ; i++){
     console.log(product);
 }
 
+// The main difference between a traditional "for" loop and a "for...in" loop lies in what they are designed to iterate over and how they operate
+
+
 //for of loop detecated by the only array in loop
 
 for (const product of products) {
     console.log('for of loop is -----', product);
 }
+
+
+// Certainly! The "for...in" loop is a programming construct that is commonly used to iterate over the properties of an object or elements of an iterable, such as an array or a map. It's available in many programming languages, including JavaScript, Python, and others.
+
+const myObject = {
+    name: "John",
+    age: 30,
+    city: "New York"
+  };
+  
+  for (const property in myObject) {
+    console.log(property + ": " + myObject[property]);
+  }
+  
+
+
